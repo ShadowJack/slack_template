aa5a9f2624b2b6fbea9e9e9cc0b85bdf285a94e6
shadowjack <kostyap@hotmail.ru>
2016-05-23 23:25:36 +0300

    Get ready for deployment

--- TODO.md
+++ TODO.md
@@ -2,5 +2,4 @@
 2. ~~Add a method that returns saved template~~
 3. Add installation section into readme
 4. ~~Add a method to list all saved templates~~
-
-mb a table with mapping slack-team - security-token will be required?
+5. Deploy on bluemix
--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,11 @@
 use Mix.Config
 
-config :logger, level: :info
+config :slack_template, slack_token: "VLeV7Dv5ChkhGvwMtjipCwkw"
 
-import_config "prod.secret.exs"
+config :slack_template, SlackTemplate.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "cbqjnumn",
+  password: "ssbGWeKAe8J_vyQg_tYX7lHdkHYhbTQo",
+  database: "cbqjnumn",
+  hostname: "qdjjtnkv.db.elephantsql.com",
+  pool_size: 10
