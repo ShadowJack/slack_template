3308b3d096b02bbf724efe1e9baad404d98e84c6
shadowjack <kostyap@hotmail.ru>
2016-05-25 00:06:50 +0300

    Fix prod env

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-config :slack_template, slack_token: "VLeV7Dv5ChkhGvwMtjipCwkw"
+config :logger, level: :info
 
-config :slack_template, SlackTemplate.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "cbqjnumn",
-  password: "ssbGWeKAe8J_vyQg_tYX7lHdkHYhbTQo",
-  database: "cbqjnumn",
-  hostname: "qdjjtnkv.db.elephantsql.com",
-  pool_size: 10
+import_config "prod.secret.exs"
